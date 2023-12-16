a155a668983043a701b97b177e0f086d290a1021
Math-Lounge <77364353+Math-Lounge@users.noreply.github.com>
2023-12-17 01:42:30 +0530

    Time: 38 ms (60.15%) | Memory: 16.4 MB (30.89%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -7,11 +7,23 @@
 class Solution:
     def inorderTraversal (self, root: Optional[TreeNode]) -> List[int]:
         if not root: return []
-        return self._inorderTraversal_ (root, list ())
+        return self._inorderTraversalIterative_ (root)
+        return self._inorderTraversalRecurse_ (root, list ())
 
-    def _inorderTraversal_ (self, root: Optional[TreeNode], order) -> List[int]:
+    def _inorderTraversalRecurse_ (self, root: Optional[TreeNode], order) -> List[int]:
         if not root: return order
-        self._inorderTraversal_ (root.left, order)
+        self._inorderTraversalRecurse_ (root.left, order)
         order.append (root.val)
-        self._inorderTraversal_ (root.right, order)
+        self._inorderTraversalRecurse_ (root.right, order)
+        return order
+
+    def _inorderTraversalIterative_ (self, root: Optional[TreeNode]) -> List[int]:
+        order, stack = [], []
+        while (len (stack) > 0) or (root is not None):
+            while (root is not None):
+                stack.append (root)
+                root = root.left
+            root = stack.pop ()
+            order.append (root.val)
+            root = root.right
         return order
