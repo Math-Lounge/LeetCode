2ea7b053702f3b6cc6797dca3d080c15ee1d0eda
Math-Lounge <77364353+Math-Lounge@users.noreply.github.com>
2023-12-18 01:34:12 +0530

    Time: 812 ms (91.72%) | Memory: 21.1 MB (15.55%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -2,21 +2,25 @@ class Solution:
 
     def threeSum (self, nums: List[int]) -> List[List[int]]:
         nums.sort ()
-        res = []
+        N = len (nums)
+        results = []
         for i, val in enumerate (nums):
             if val > 0: break
-            if (i == 0) or (val != nums [i-1]):
-                self.twoSum (nums, i, res)
-        return res
+            # Remaining numbers are positive
+            # and can't add up to a neg number
+            if (i > 0) and (nums [i] == nums [i-1]): continue
+            # We have already seen this number before
+            self.twoSum (nums, -val, i+1, N-1, results)
+        return results
 
-    def twoSum (self, nums, i, res):
-        L, R = i+1, len (nums) - 1
-        while (L < R):
-            T = nums [L] + nums [R] + nums [i]
-            if (T == 0):
-                res.append ([ nums [i], nums [L], nums [R], ])
-                L += 1; R -= 1;
+    def twoSum (self, nums, target, L, R, results):
+        while L < R:
+            S = nums [L] + nums [R]
+            if  (S == target):
+                results.append ([ -target, nums [L], nums [R], ])
+                L, R = L+1, R-1
+                # But both L & R could be repeated values
                 while (L < R) and (nums [L] == nums [L-1]): L += 1
                 while (L < R) and (nums [R] == nums [R+1]): R -= 1
-            elif (T < 0): L += 1
-            elif (T > 0): R -= 1
+            elif (S < target): L += 1
+            elif (S > target): R -= 1
