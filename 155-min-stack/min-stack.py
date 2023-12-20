3a31bc7aa3aa95a2a8f6a45d87559debfc8a8e6e
Math-Lounge <77364353+Math-Lounge@users.noreply.github.com>
2023-12-20 18:09:02 +0530

    Time: 50 ms (97.04%) | Memory: 21.2 MB (5.34%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -1,25 +1,32 @@
 class MinStack:
 
     def __init__ (self):
-        self.min = []
+        self.min = [] # (min value, count) tuple
         self.stack = []
 
     def push (self, val) -> None:
-        if (len (self.min) == 0) or (val <= self.min [-1]):
-            self.min.append (val)
+        if (len (self.min) == 0):
+            self.min.append ((val, 1))
+        elif (val < self.min [-1] [0]):
+            self.min.append ((val, 1))
+        elif (val == self.min [-1] [0]):
+            count = self.min.pop () [1]
+            self.min.append ((val, count + 1))
         self.stack.append (val)
 
     def pop (self) -> None:
         if len (self.stack) == 0: return
         val = self.stack.pop ()
-        if (val == self.min [-1]):
-            self.min.pop ()
+        if (val == self.min [-1] [0]):
+            count = self.min.pop () [1]
+            if count > 1:
+                self.min.append ((val, count-1))
 
     def top (self) -> int:
         return self.stack [-1]
 
     def getMin (self) -> int:
-        return self.min [-1]
+        return self.min [-1] [0]
 
 class Old:
 
