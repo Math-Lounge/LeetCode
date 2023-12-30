bf636ae72c02718d7b5d5a3988c1c9ecfd034f76
Math-Lounge <77364353+Math-Lounge@users.noreply.github.com>
2023-12-30 15:22:22 -0500

    Time: 143 ms (5.23%) | Memory: 18.5 MB (21.13%) - LeetSync

---
+++ 120-triangle/triangle.py
@@ -0,0 +1,16 @@
+class Solution:
+
+    # path[level] = min(tri[level,:])
+    # tri[i,j] = tri[i,j].value + min(tri[i-1,j], tri[i-1,j-1], tri[i-1,j+1])
+
+    def minimumTotal (self, triangle: List[List[int]]) -> int:
+        N = len (triangle)
+        last = triangle [0]
+        for level in range (1, N):
+            path = [0] * (level+1)
+            path [0] = last [0] + triangle [level] [0]
+            for cell in range (1, level):
+                path [cell] = triangle [level] [cell] + min (last [cell-1], last [cell])
+            path [level] = triangle [level] [level] + last [level-1]
+            last = path
+        return min (last)
