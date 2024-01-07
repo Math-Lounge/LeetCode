6111bdc1dee8d70d5b545340b0655dfb68dd9c92
Math-Lounge <77364353+Math-Lounge@users.noreply.github.com>
2024-01-07 00:44:07 -0500

    Time: 2780 ms (5.07%) | Memory: 37.6 MB (9.15%) - LeetSync

--- 1738-maximal-network-rank/maximal-network-rank.py
+++ 1738-maximal-network-rank/maximal-network-rank.py
@@ -8,9 +8,10 @@ class Solution:
             graph [r,c] = True
             graph [c,r] = True
         network_rank = 0
+        cached = np.array ([ graph [r, :] for r in range (n) ])
         for r in range (n):
             for c in range (r+1, n):
-                rc_rank = sum (graph [r, :]) + sum (graph [:, c]) - graph [r, c]
+                rc_rank = sum (cached [r]) + sum (cached [c]) - graph [r, c]
                 network_rank = max (network_rank, rc_rank)
         print (graph.astype (int))
         return network_rank
